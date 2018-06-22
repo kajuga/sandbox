85f7033c8188671449f4acc874a871bda4c4a1f7
kajuga <msg2fedorov@gmail.com>
2018-06-22 13:43:33 +0300

    Demonstrate HashSet.

---
+++ src/main/java/laboratory/stack/collections/HashSetDemo.java
@@ -0,0 +1,20 @@
+package laboratory.stack.collections;
+
+import java.util.*;
+
+class HashSetDemo {
+    public static void main(String args[]) {
+        // Create a hash set.
+        HashSet<String> hs = new HashSet<String>();
+
+        // Add elements to the hash set.
+        hs.add("Beta");
+        hs.add("Alpha");
+        hs.add("Eta");
+        hs.add("Gamma");
+        hs.add("Epsilon");
+        hs.add("Omega");
+
+        System.out.println(hs);
+    }
+}
