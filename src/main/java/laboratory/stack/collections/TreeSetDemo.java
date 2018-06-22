ad676b7fc3957c33132057e3fb97defb43d50d38
kajuga <msg2fedorov@gmail.com>
2018-06-22 13:49:06 +0300

    Demonstrate Demonstrate TreeSet.

---
+++ src/main/java/laboratory/stack/collections/TreeSetDemo.java
@@ -0,0 +1,20 @@
+package laboratory.stack.collections;
+
+import java.util.TreeSet;
+
+class TreeSetDemo {
+    public static void main(String args[]) {
+        // Create a tree set.
+        TreeSet<String> ts = new TreeSet<String>();
+
+        // Add elements to the tree set.
+        ts.add("C");
+        ts.add("A");
+        ts.add("B");
+        ts.add("E");
+        ts.add("F");
+        ts.add("D");
+
+        System.out.println(ts);
+    }
+}
