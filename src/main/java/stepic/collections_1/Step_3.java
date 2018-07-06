69d783a8e4e8e85bd53c05a2530a437c73481e3f
kajuga <msg2fedorov@gmail.com>
2018-07-06 17:06:22 +0300

    Stepic simple course of Collection framework добавил решений из JAVA 8.

--- src/main/java/stepic/collections_1/Step_3.java
+++ src/main/java/stepic/collections_1/Step_3.java
@@ -5,18 +5,27 @@ import java.util.TreeMap;
 
 public class Step_3 {
     public static void main(String[] args) {
-
-
         Map<String, Integer> map = new TreeMap<>();
         map.put("Gamma",  3);
         map.put("Omega", 24);
         map.put("Alpha",  1);
 
+        //JAVA 8:
+        map.entrySet().forEach(System.out::println);
 
-        //write your code here
+        //ЕЩЕ 8:
+        map.entrySet().stream().forEach(System.out::println);
 
+        //ЕЩЕ 8:
+        map.forEach((x,y) -> System.out.println(x + "=" + y));
+
+        //ЕЩЕ 8:
+        map.entrySet().forEach(entry -> System.out.println(String.format("%s=%d", entry.getKey(), entry.getValue())));
+
+        //По старинке:
         for(Map.Entry <String, Integer> entry : map.entrySet()){
             System.out.println(entry.getKey() + "=" + entry.getValue());
         }
     }
 }
+
