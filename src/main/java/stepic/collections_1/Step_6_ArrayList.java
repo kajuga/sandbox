051f9117ee0e4110a3c2cf30db7b90aef0b5a845
kajuga <msg2fedorov@gmail.com>
2018-07-06 20:33:45 +0300

    Stepic simple course of Collection framework: ArrayList

---
+++ src/main/java/stepic/collections_1/Step_6_ArrayList.java
@@ -0,0 +1,31 @@
+package stepic.collections_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Step_6_ArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(0);
+        list.add(1);
+        list.add(7);
+
+        //also
+        List<Integer> list2 = Arrays.asList(2, 0, 1, 7);
+
+        System.out.println(list);
+        System.out.println(list2);
+
+        //Или так
+        List<Integer> list3 =new ArrayList<Integer>(){
+            {
+                add(2);
+                add(0);
+                add(1);
+                add(7);
+            }
+        };
+    }
+}
