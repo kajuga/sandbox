d63040798b13e8427acc28dc885aa4ae62fd70b7
kajuga <msg2fedorov@gmail.com>
2018-07-11 08:57:02 +0300

    simply sample test splitter

--- src/main/java/testing/SplitterTester.java
+++ src/main/java/testing/SplitterTester.java
@@ -1,6 +1,6 @@
 package testing;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class SplitterTester {
     public static void main(String[] args) {
@@ -12,5 +12,11 @@ public class SplitterTester {
         for (String str: token) {
             System.out.println(str);
         }
+
+        List<String> sortedList = Arrays.asList(token);
+        sortedList.sort(String::compareToIgnoreCase);
+        for (String ssstttrr : sortedList) {
+            System.out.println(ssstttrr);
+        }
+        }
     }
-}
