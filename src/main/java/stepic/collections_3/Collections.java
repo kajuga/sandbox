c358f63db3dc5c140d15c104c82fd5d3b20ce30a
kajuga <msg2fedorov@gmail.com>
2018-07-12 12:29:07 +0300

    simply sample Collections interface

---
+++ src/main/java/stepic/collections_3/Collections.java
@@ -0,0 +1,14 @@
+package stepic.collections_3;
+
+public interface Collections <E> extends Iterable {
+
+    int size();
+    boolean isEmpty();
+    boolean contains(Object o);
+    boolean add();
+    boolean remove();
+    void clear();
+
+
+
+}
