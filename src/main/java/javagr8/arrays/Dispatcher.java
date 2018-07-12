f5471ca148814d69f3ec0cf3e2f8714e19be0026
kajuga <msg2fedorov@gmail.com>
2018-07-12 17:28:39 +0300

    JAVAGR8 lessob binary search for array example

---
+++ src/main/java/javagr8/arrays/Dispatcher.java
@@ -0,0 +1,15 @@
+package javagr8.arrays;
+
+import java.util.Arrays;
+
+public class Dispatcher {
+    public static void main(String[] args) {
+        int [] x = {4,3,8,5,1,7,0,2}; //0 1 2 3 4 5 7 8
+        Arrays.sort(x);
+        int index = Arrays.binarySearch(x, 3);
+        System.out.println(index);
+
+    }
+
+}
+// -n -1
