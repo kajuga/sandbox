017bdde09f6ac845214133b9960d0eda943170f4
kajuga <msg2fedorov@gmail.com>
2018-07-15 18:05:42 +0300

    JAVA Advanced List in

--- src/main/java/advanced_Java/arrays/LinkedList_Advanced.java
+++ src/main/java/advanced_Java/arrays/LinkedList_Advanced.java
@@ -1,4 +1,18 @@
 package advanced_Java.arrays;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedList_Advanced {
+    public static void main(String[] args) {
+
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(10);
+
+        System.out.println(myLinkedList);
+
+    }
 }
