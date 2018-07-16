521ce7a5d02b0e8a5268099c53938a0b883d387e
kajuga <msg2fedorov@gmail.com>
2018-07-16 09:54:06 +0300

    JAVA Advanced Setz simple examples

---
+++ src/main/java/advanced_Java/setz/Setz.java
@@ -0,0 +1,27 @@
+package advanced_Java.setz;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Setz {
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet <>();
+        Set<String> linkedHashSet = new LinkedHashSet <>();
+        Set<String> treeSet = new TreeSet <>();
+
+        hashSet.add("Katy");
+        hashSet.add("Mike");
+        hashSet.add("Tom");
+        hashSet.add("George");
+        hashSet.add("Donald");
+        hashSet.add("Tom");
+        hashSet.add("Tom");
+
+
+
+
+    }
+
+}
