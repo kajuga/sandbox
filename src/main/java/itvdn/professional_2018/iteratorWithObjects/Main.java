13cf038169020831ebc48d218356e34f287d16d8
kajuga <msg2fedorov@gmail.com>
2018-11-05 16:40:07 +0300

    ITVDN Iterators & Listreators II

---
+++ src/main/java/itvdn/professional_2018/iteratorWithObjects/Main.java
@@ -0,0 +1,20 @@
+package itvdn.professional_2018.iteratorWithObjects;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Animal> al = new ArrayList <>();
+        al.add(new Animal("Sphynx", 5000));
+        al.add(new Animal("Malamute", 4000));
+
+        Iterator<Animal> carIterator = al.iterator();
+        while (carIterator.hasNext()) {
+            Animal tmp = carIterator.next();
+            tmp.price += 1000;
+        }
+        System.out.println(al);
+
+    }
+}
