e13a81a02be8204835a3ad04dff352b6a07e4c94
kajuga <msg2fedorov@gmail.com>
2018-11-05 21:10:14 +0300

    ITVDN  Comaprable on Objects

---
+++ src/main/java/itvdn/professional_2018/comparators/comparableStudent/SimpleComparableStudent.java
@@ -0,0 +1,48 @@
+package itvdn.professional_2018.comparators.comparableStudent;
+
+import java.util.Arrays;
+
+public class SimpleComparableStudent implements Comparable{
+    private String firstName;
+    private int group;
+
+    public SimpleComparableStudent(String firstName, int group) {
+        this.firstName = firstName;
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + group;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (this.group > ((SimpleComparableStudent)o).group) {
+            return 1;
+        }
+        if (this.group < ((SimpleComparableStudent)o).group) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+
+    }
+}
+
+    class Main {
+    public static void main(String[] args) {
+         SimpleComparableStudent[] myStudents = {
+                 new SimpleComparableStudent("Evgen", 333),
+                 new SimpleComparableStudent("Aaron", 1233),
+                 new SimpleComparableStudent("Caya", 62233),
+                 new SimpleComparableStudent("Deryl", 432)};
+
+        Arrays.sort(myStudents);
+        for (SimpleComparableStudent myStudent : myStudents) {
+            System.out.println(myStudent);
+        }
+    }
+
+}
