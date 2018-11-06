98855e8c0f82a3c65021353de99a948f1767cc95
kajuga <msg2fedorov@gmail.com>
2018-11-06 12:03:42 +0300

    HomeWork доразобраться где косячу

---
+++ src/main/java/itvdn/professional_2018/homeWork/Animal.java
@@ -0,0 +1,19 @@
+package itvdn.professional_2018.homeWork;
+
+public class Animal {
+    String breed;
+    int price;
+
+    public Animal(String breed, int price) {
+        this.breed = breed;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "breed='" + breed + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/java/itvdn/professional_2018/homeWork/MyList.java
@@ -0,0 +1,50 @@
+package itvdn.professional_2018.homeWork;
+
+import java.util.*;
+
+public class MyList {
+//    public static void main(String[] args) {
+//
+//        List<Animal> myList = new ArrayList <>();
+//
+//        myList.add(new Animal("Popo", 5000));
+//        myList.add(new Animal("Mumu", 10000));
+//        myList.add(new Animal("Ggaga", 2000));
+//
+//        //реализовал итератор
+//        Iterator<Animal> itr = myList.iterator();
+//        while (itr.hasNext()) {
+//            System.out.println(itr.next());
+//        }
+//        Animal[] tmp = myList.toArray();
+//
+//        //реализация компаратора припомощи анонимного класса
+//
+//        Arrays.sort(tmp, new Comparator <Animal>() {
+//            public int compare(Animal o1, Animal o2) {
+//                return o1.price - o2.price;
+//            }
+//        });
+//
+//        for (int i = 0; i < tmp.length; i++) {
+//            System.out.println(tmp[i]);
+//        }
+//
+//    }
+
+
+}
+
+
+
+/*
+        Arrays.sort(c, new Comparator <Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                return o1.price - o2.price;
+            }
+        });
+
+Вывод на экран элементов List:
+Создать список, заполнить его на 10 элементов и вывести на экран содержимое используя iterator.
+ */
