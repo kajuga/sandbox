03857dc5211e9f517af5e0bcfff2f7b88b9e6a28
kajuga <msg2fedorov@gmail.com>
2018-11-21 20:12:42 +0300

    Computer Science Centre Courses - Multithreading

---
+++ src/main/java/CSC/multithreading/demo1/Demo1.java
@@ -0,0 +1,30 @@
+package CSC.multithreading.demo1;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; ++i) {
+//            new HelloThread().start();
+            new Thread(new HelloRunnable()).start();
+        }
+
+        System.out.println("Hello from main thread");
+    }
+
+
+    private static class HelloThread extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("Hello from " + getName());
+        }
+    }
+
+    private static class HelloRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Hello from " + Thread.currentThread().getName());
+        }
+    }
+}
