b3150613fea12de75feb310f7096935c6f44a11c
kajuga <msg2fedorov@gmail.com>
2018-11-21 16:08:13 +0300

    Computer Science Centre Courses - Multithreading

---
+++ src/main/java/CSC_Miltithreading/multithreading/demo3/Account.java
@@ -0,0 +1,41 @@
+package CSC_Miltithreading.multithreading.demo3;
+
+public class Account {
+
+    private long balance;
+
+    public Account() {
+        this(0L);
+    }
+
+    Account(long balance) {
+        this.balance = balance;
+    }
+
+    long getBalance() {
+        return balance;
+    }
+
+    void deposit(long amount) {
+        checkAmountNonNegative(amount);
+        synchronized (this) {
+            balance += amount;
+        }
+    }
+
+    void withdraw(long amount) {
+        checkAmountNonNegative(amount);
+        synchronized (this) {
+            if (balance < amount) {
+                throw new IllegalArgumentException("not enough money");
+            }
+            balance -= amount;
+        }
+    }
+
+    private static void checkAmountNonNegative(long amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("negative amount");
+        }
+    }
+}
---
+++ src/main/java/CSC_Miltithreading/multithreading/demo4/Demo4.java
@@ -0,0 +1,33 @@
+package CSC_Miltithreading.multithreading.demo4;
+
+public class Demo4 {
+
+    public static void main(String[] args) throws Exception {
+        Account account = new Account(0);
+
+        new DepositThread(account).start();
+
+        System.out.println("Calling waitAndWithdraw()...");
+
+        account.waitAndWithdraw(50_000_000);
+
+        System.out.println("waitAndWithdraw finished, end balance = " + account.getBalance());
+    }
+
+
+    private static class DepositThread extends Thread {
+
+        private final Account account;
+
+        private DepositThread(Account account) {
+            this.account = account;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 50_000_000; ++i) {
+                account.deposit(1);
+            }
+        }
+    }
+}
