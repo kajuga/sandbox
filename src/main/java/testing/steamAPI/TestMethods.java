36bac6217bbb351cca8c51d29e74d6acf88d3b57
kajuga <kajuga@mail.ru>
2019-01-04 14:45:05 +0300

    Stream API ex.2 adds

--- src/main/java/testing/steamAPI/TestMethods.java
+++ src/main/java/testing/steamAPI/TestMethods.java
@@ -1,5 +1,8 @@
 package testing.steamAPI;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class TestMethods {
@@ -14,9 +17,19 @@ public class TestMethods {
             System.out.println(count);
         System.out.println("================");
 
-
         long counter = IntStream.of(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5).filter(w -> w > 0).count();
         System.out.println(counter);
+
+
+        List <String> list = new ArrayList <>();
+        Collections.addAll(list, "Moscow", "London", "Volgograd", "Mihailovkha");
+        list.stream()       // получаем поток
+                .filter(s -> s.length() > 6)    // применяем фильтрацию по длине
+                .forEach(s -> System.out.println(s)); // выводим отфильтрованные строки на консоль
+
+
+
+
 
         }
 
