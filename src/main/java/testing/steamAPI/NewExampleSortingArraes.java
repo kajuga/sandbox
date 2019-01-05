ebd2626cc3820f888377abd997cf429a7083fc75
kajuga <kajuga@mail.ru>
2019-01-05 09:52:13 +0300

    Stream API ex.5 adds object example

--- src/main/java/testing/steamAPI/NewExampleSortingArraes.java
+++ src/main/java/testing/steamAPI/NewExampleSortingArraes.java
@@ -1,8 +1,10 @@
 package testing.steamAPI;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class NewExampleSortingArraes {
     public static void main(String[] args) {
@@ -17,6 +19,21 @@ public class NewExampleSortingArraes {
                 .filter(p->p.length()<12)
                 .sorted() // сортировка по возрастанию
                 .forEach(s->System.out.println(s));
+        System.out.println("==================================================================");
+        Stream<String> phoneStream = Stream.of("iPhone 6 S", "Lumia 950", "Samsung Galaxy S 6", "LG G 4", "Nexus 7");
+
+        phoneStream.skip(1)
+                .limit(2)
+                .forEach(s->System.out.println(s));
+
+
+        System.out.println("=============================");
+        ArrayList<String> names = new ArrayList<String>();
+        names.addAll(Arrays.asList(new String[]{"Tom", "Sam", "Bob", "Alice"}));
+        System.out.println(names.stream().count());  // 4
+
+        // количество элементов с длиной не больше 3 символов
+        System.out.println(names.stream().filter(n->n.length()<=3).count());  // 3
     }
 
 }
