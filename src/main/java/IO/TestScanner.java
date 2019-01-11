dc31e4d9ab41095b5ce3bb05d539d50422b29559
kajuga <kajuga@mail.ru>
2019-01-11 21:00:04 +0300

    Some strings experiments

---
+++ src/main/java/IO/TestScanner.java
@@ -0,0 +1,18 @@
+package IO;
+
+import java.util.Scanner;
+
+public class TestScanner {
+    public static void main(String[] args) {
+        String str =  "ussr";
+        String str2 = str.toUpperCase();
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str);
+
+
+
+
+    }
+
+}
