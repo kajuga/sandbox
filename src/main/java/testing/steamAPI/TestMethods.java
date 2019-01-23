68a457c79c30c36342bb048acda95968dbced092
kajuga <kajuga@mail.ru>
2019-01-23 21:01:50 +0300

    ChatBot

--- src/main/java/testing/steamAPI/TestMethods.java
+++ src/main/java/testing/steamAPI/TestMethods.java
@@ -8,34 +8,8 @@ import java.util.stream.Stream;
 
 public class TestMethods {
     public static void main(String[] args) {
-        int[] numbers = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
-        int count = 0;
-        for(int i : numbers) {
-            if (i > 0) {
-                ++count;
-            }
-        }
-            System.out.println(count);
-        System.out.println("================");
-        long counter = IntStream.of(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5).filter(w -> w > 0).count();
-        System.out.println(counter);
-
-        List <String> list = new ArrayList <>();
-        Collections.addAll(list, "Moscow", "London", "Volgograd", "Mihailovkha");
-        list.stream()       // получаем поток
-                .filter(s -> s.length() > 6)    // применяем фильтрацию по длине
-                .forEach(s -> System.out.println(s)); // выводим отфильтрованные строки на консоль
-
-        System.out.println("::::::::::::::::::::::::::::::::::::::");
-
-        Stream<String> streamstr = list.stream();
-//        streamstr.filter(s -> s.length() == 6);
-//        streamstr.forEach(s -> System.out.println(s));
-
-        Stream<String> citiesStream = Stream.of("Париж", "Лондон", "Мадрид","Берлин", "Брюссель");
-        Stream<Integer> citiesStream2 = Stream.of(2, 3, 6,34, 44);
-        citiesStream.forEach(System.out::println);
-        citiesStream2.forEach(System.out::print);
+        short x = 4 ;
+        System.out.print((x > 4) ? 99.9 : 9);
 
 
 
---
+++ src/main/java/testing/Вшызфесрук.java
@@ -0,0 +1,18 @@
+package testing;
+
+public class Вшызфесрук {
+
+
+    public int Counter(int start, int finish) {
+
+        finish = 10;
+        int sum = 10;
+for(start =1; start<=finish; start++) {
+ if(start % 2 == 0) {
+     sum += start;
+ }
+}
+return sum;
+    }
+
+}
