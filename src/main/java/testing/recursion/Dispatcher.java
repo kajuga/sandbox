3df6c4005b52540346e6e2fa955f8889e1058c75
kajuga <kajuga@mail.ru>
2019-02-02 12:07:40 +0300

    Рекурсия ручками факториал

--- src/main/java/testing/recursion/Dispatcher.java
+++ src/main/java/testing/recursion/Dispatcher.java
@@ -6,6 +6,20 @@ public class Dispatcher {
         a.print(10);
         System.out.println();
         a.print(5);
+        System.out.println();
+        System.out.println("==");
+        System.out.println();
+        //факториал рекурсией
+        int res = fact(5);
+        System.out.println("res: " + res);
     }
+
+//Вычисляю факториал - рекурсия
+    public static int fact(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        return fact(n -1) * n;
+    }
+
 
 }
