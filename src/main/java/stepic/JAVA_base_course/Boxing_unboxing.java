57bd141a3796feff2ffda85a54fd9f8244694c50
kajuga <kajuga@mail.ru>
2019-03-23 17:21:53 +0300

    2.2 Преобразование типов

---
+++ src/main/java/stepic/JAVA_base_course/Boxing_unboxing.java
@@ -0,0 +1,33 @@
+package stepic.JAVA_base_course;
+
+/**
+ * boxing/unbpxing primitives - вручную
+ * примитивы не поддерживабстя коллекциями
+ */
+
+public class Boxing_unboxing {
+
+    public static void main(String[] args) {
+
+        int primitive = 0;
+
+        Integer reference = Integer.valueOf(primitive);
+        System.out.println(reference);
+
+        int  unboxing = reference.intValue();
+        System.out.println(unboxing);
+
+/**
+ * Преоббразование в строку/из строки
+ */
+        int fromString = Integer.parseInt("666");
+        System.out.println(fromString);
+
+        String toString = Integer.toString(123);
+        System.out.println(toString);
+
+
+
+
+    }
+}
