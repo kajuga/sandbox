825e2cd4f940047f84d86115e6a13e9eb7d979e0
kajuga <kajuga@mail.ru>
2019-03-25 14:46:26 +0300

    Merges two given sorted arrays into one all done some refactoring code

--- src/main/java/stepic/JAVA_base_course/MergedTwoArrays_2_4.java
+++ src/main/java/stepic/JAVA_base_course/MergedTwoArrays_2_4.java
@@ -9,8 +9,8 @@ package stepic.JAVA_base_course;
 public class MergedTwoArrays_2_4 {
 
     public static void main(String[] args) {
-        int[] a1 = {0, 2, 2, 4, 5, 6, 7};
-        int[] a2 = {1, 3, 3, 3};
+        int[] a1 = {2, 2, 2, 4, 5, 6, 7};
+        int[] a2 = {1, 3, 4, 5, 6};
         int[] union = mergeArrays(a1, a2);
         for (int i = 0; i < union.length; i++) {
             System.out.print(union[i] + " ");
@@ -18,34 +18,15 @@ public class MergedTwoArrays_2_4 {
     }
 
     public static int[] mergeArrays(int[] a1, int[] a2) {
-        int temp1 = a1.length;
-        int temp2 = a2.length;
-        int mergedLength = temp1 + temp2;
-
-        int[] merged = new int[mergedLength];
         int i = 0, j = 0;
+        int[] res = new int[a1.length + a2.length];
 
-        for (int k = 0; k < mergedLength; k++) {
-            if (i > a1.length - 1) {
-                int a = a2[j];
-                merged[k] = a;
-                j++;
-        }
-        else if (j > a2.length - 1) {
-                int a = a1[i];
-                merged[k] = a;
-                i++;
-            }
-            else if (a1[i] < a2[j]) {
-                int a = a1[i];
-                merged[k] = a;
-                i++;
-            } else {
-                int b = a2[j];
-                merged[k] = b;
-                j++;
-            }
-        }
-        return merged;
+        while (i < a1.length && j < a2.length)
+            res[i + j] = (a1[i] < a2[j]) ? a1[i++] : a2[j++];
+        while (i < a1.length)
+            res[i + j] = a1[i++];
+        while (j < a2.length)
+            res[i + j] = a2[j++];
+        return res;
     }
 }
