46497e12a568c9f14836fc09a818951e330e47c3
kajuga <kajuga@mail.ru>
2019-04-06 20:22:22 +0300

    Task 4.1 Знакомство с исключениями

---
+++ src/main/java/stepic/JAVA_base_course/Sqrt_4_1.java
@@ -0,0 +1,17 @@
+package stepic.JAVA_base_course;
+
+/*
+Реализуйте метод sqrt(), вычисляющий квадратный корень числа. В отличие от Math.sqrt(),
+это метод при передаче отрицательного параметра должен бросать исключение java.lang.IllegalArgumentException с
+сообщением "Expected non-negative number, got ?", где вместо вопросика будет подставлено фактически переданное в метод число.
+ */
+
+public class Sqrt_4_1 {
+
+
+    public static double sqrt(double x) {
+        if (x < 0) throw new IllegalArgumentException("Expected non-nagative number, got " + x);
+        return Math.sqrt(x);
+    }
+
+}
