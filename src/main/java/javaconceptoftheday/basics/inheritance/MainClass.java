b378e1299eb2deb3c64b0c5003dd427701bee45c
kajuga <Ff14789kldd>
2019-04-29 13:51:12 +0300

    test  push

---
+++ src/main/java/javaconceptoftheday/basics/inheritance/MainClass.java
@@ -0,0 +1,27 @@
+package javaconceptoftheday.basics.inheritance;
+
+class A{
+    int i = 1212;
+}
+
+class B extends A{
+    A a;
+
+    public B(A a){
+        this.a = a;
+    }
+}
+
+public class MainClass{
+    public static void main(String[] args)    {
+        A a = new A();
+        B b = new B(a);
+        System.out.println(a.i);
+        System.out.println(b.i);
+        System.out.println(b.a.i);
+        b.a.i = 2121;
+        System.out.println("--------");
+        System.out.println(a.i);
+        System.out.println(b.i);
+    }
+}
